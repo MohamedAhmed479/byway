25dad6512e217d0494e620fb2f756e63cc3293e6
MohamedAhmedDev <mohamedahmeddev333@gmail.com>
2025-09-20 07:00:47 +0300

    Define empty migrations & models files

---
+++ app/Database/Migrations/2025-09-20-034448_CreatePaymentItemsTable.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreatePaymentItemsTable extends Migration
+{
+    public function up()
+    {
+        //
+    }
+
+    public function down()
+    {
+        //
+    }
+}
